f89852af1925f43a729963edc1d5f80758ae5227
t8d7r <t8d7r@protonmail.com>
2017-05-07 08:24:07 -0400

    update

--- actions/test.py
+++ actions/test.py
@@ -1,7 +1,11 @@
 import sys
 import os
+from st2client.client import Client
 from st2actions.runners.pythonrunner import Action
-try:
-	print str(sys.argv[1])+str(sys.argv[2])+str(sys.argv[3])
-except:
-	exit(1)
+
+class InternalDelIntf(Action):
+	def run(self, chain1, chain2, chain3):
+		try:
+			print str(sys.argv[1])+str(sys.argv[2])+str(sys.argv[3])
+		except:
+			return(1)
