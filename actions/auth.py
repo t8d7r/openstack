77b17c8b9eaa16b603d078629bb07d90ed46e070
t8d7r <t8d7r@protonmail.com>
2017-05-07 11:04:44 -0400

    update

--- actions/auth.py
+++ actions/auth.py
@@ -11,21 +11,17 @@ class InternalDelIntf(Action):
 	def run(self, auth_user, auth_password, auth_project, auth_ip):
 		client = Client()
 		url = 'http://'+auth_ip+':5000/v3/auth/tokens'
-		print url
 		payload = { 'auth': {'identity': {'methods': ['password'],'password': {'user': {'name': auth_user,'domain': { 'id': 'default' },'password': auth_password}}},'scope': {'project': {'name': auth_project,'domain': { 'id': 'default' }}}}}
-		print json.dumps(payload)
 		try:
 			r=requests.post(url, data=json.dumps(payload))
-			print r
 			token=r.headers['X-Subject-Token']
-			print token
-			client.keys.update(KeyValuePair(name='token', value=token))
-			client.keys.update(KeyValuePair(name='user', value=user))
-			client.keys.update(KeyValuePair(name='project', value=project))
+			client.keys.update(KeyValuePair(name='curr_token', value=token))
+			client.keys.update(KeyValuePair(name='curr_user', value=auth_user))
+			client.keys.update(KeyValuePair(name='curr_project', value=auth_project))
 			keys = client.keys.get_all()
-			print(client.keys.get_by_name(name='token'))
-			print(client.keys.get_by_name(name='user'))
-			print(client.keys.get_by_name(name='project'))
+			print(client.keys.get_by_name(name='curr_token'))
+			print(client.keys.get_by_name(name='curr_user'))
+			print(client.keys.get_by_name(name='curr_project'))
 			
 		except:
 			return(1)
