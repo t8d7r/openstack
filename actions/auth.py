ff628eee09b64dede75874bda39cf27647ee1fe9
t8d7r <t8d7r@protonmail.com>
2017-05-07 10:36:43 -0400

    update

--- actions/auth.py
+++ actions/auth.py
@@ -11,11 +11,11 @@ class InternalDelIntf(Action):
 	def run(self, auth_user, auth_password, auth_project, auth_ip):
 		client = Client()
 		url = 'http://'+auth_ip+'/v3/auth/tokens'
-		print url
 		payload = { "auth": {'identity': {'methods': ['password'],'password': {'user': {'name': auth_user,'domain': { 'id': 'default' },'password': auth_password}}},'scope': {'project': {'name': auth_project,'domain': { 'id': 'default' }}}}}
 		try:
 			r=requests.post(url, data=json.dumps(payload))
 			token=r.headers['X-Subject-Token']
+			print token
 			client.keys.update(KeyValuePair(name='token', value=token))
 			client.keys.update(KeyValuePair(name='user', value=user))
 			client.keys.update(KeyValuePair(name='project', value=project))
