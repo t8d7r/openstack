d63d2559c6feb0b7b9b773beac41ec493e34850a
t8d7r <t8d7r@protonmail.com>
2017-05-08 12:37:52 -0400

    update

--- actions/serverdelete.py
+++ actions/serverdelete.py
@@ -8,20 +8,15 @@ from st2actions.runners.pythonrunner import Action
 from st2client.models import KeyValuePair
 
 class InternalDelIntf(Action):
-	def run(self, auth_ip, serv_name, secgrp_id, flav_id, image_id, nets):
-		i = 0
-		paytemp = "["
+	def run(self, serv_id, auth_ip):
 		client = Client()
 		keys = client.keys.get_all()
 		proj_id = client.keys.get_by_name(name='curr_project_id').value
 		curr_token = client.keys.get_by_name(name='curr_token').value
-		print (curr_token)
-		url = 'http://'+auth_ip+':8774/v2.1/'+proj_id+'/servers'
-		payload = {"server": {"name": serv_name,"imageRef": image_id,"flavorRef": flav_id,"security_groups": [{"name": secgrp_id}], }}
-		payload ['server']['networks'] = [dict(uuid=netid) for netid in nets]
+		url = 'http://'+auth_ip+':8774/v2.1/'+proj_id+'/servers/'+serv_id
 		headers = {'X-Auth-Token': curr_token , 'Content-Type': 'application/json' }
 		try:
-			r=requests.post(url, data=json.dumps(payload), headers=headers)
+			r=requests.delete(url, headers=headers)
 			print r
 		except:
 			return(1)
