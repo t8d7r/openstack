8bbd7e8b117e7492347a0389690163b14845f3ce
t8d7r <t8d7r@protonmail.com>
2017-05-08 03:30:31 -0400

    update

---
+++ actions/getservers.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import json
+import requests
+
+from st2client.client import Client
+from st2actions.runners.pythonrunner import Action
+from st2client.models import KeyValuePair
+
+class InternalDelIntf(Action):
+	def run(self, serv_id, auth_ip):
+		i = 0
+		client = Client()
+		url = 'http://'+auth_ip+'/v2.1/'+{{st2kv.curr_project_id}}+'/servers'
+		headers = {'X-Auth-Token':{{st2kv.curr_token}} }
+		try:
+			r = requests.get(url, headers=headers)
+			token=r.json()
+			length = len(token['servers'])
+			if serv_id == "default" :
+				while i < length :
+					print token['servers'][i]['name']+":"+token['servers'][i]['id']
+					i = i + 1
+			else :
+				while i < length :
+					if token['servers'][i]['id'] == serv_id :              
+ 						print token['servers'][i]['name']+":"+token['servers'][i]['id']
+                			i = i + 1	
+		except:
+			return(1)
+
+
+
+
