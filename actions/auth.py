eee9c3c9a3b23de61d6d01deafd0d1daf890d3db
t8d7r <t8d7r@protonmail.com>
2017-05-08 02:35:00 -0400

    update

--- actions/auth.py
+++ actions/auth.py
@@ -14,14 +14,16 @@ class InternalDelIntf(Action):
 		payload = { 'auth': {'identity': {'methods': ['password'],'password': {'user': {'name': auth_user,'domain': { 'id': 'default' },'password': auth_password}}},'scope': {'project': {'name': auth_project,'domain': { 'id': 'default' }}}}}
 		try:
 			r=requests.post(url, data=json.dumps(payload))
-			token=r.headers['X-Subject-Token']
-			client.keys.update(KeyValuePair(name='curr_token', value=token))
+			token=r.json()
+			client.keys.update(KeyValuePair(name='curr_token', value=r.headers['X-Subject-Token']))
 			client.keys.update(KeyValuePair(name='curr_user', value=auth_user))
 			client.keys.update(KeyValuePair(name='curr_project', value=auth_project))
+			client.keys.update(KeyValuePair(name='curr_project_id', value=token['token']['project']['id']))
 			keys = client.keys.get_all()
 			print(client.keys.get_by_name(name='curr_token'))
 			print(client.keys.get_by_name(name='curr_user'))
 			print(client.keys.get_by_name(name='curr_project'))
+			print(client.keys.get_by_name(name='curr_project_id'))
 			
 		except:
 			return(1)
