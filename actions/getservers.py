ee6b19b04bcfc87ec26a5b458a10c0e31f65fbb6
t8d7r <t8d7r@protonmail.com>
2017-05-08 03:39:11 -0400

    update

--- actions/getservers.py
+++ actions/getservers.py
@@ -8,11 +8,11 @@ from st2actions.runners.pythonrunner import Action
 from st2client.models import KeyValuePair
 
 class InternalDelIntf(Action):
-	def run(self, serv_id, auth_ip):
+	def run(self, serv_id, proj_id, curr_token, auth_ip):
 		i = 0
 		client = Client()
-		url = 'http://'+auth_ip+'/v2.1/'+{{st2kv.system.curr_project_id}}+'/servers'
-		headers = {'X-Auth-Token':{{st2kv.system.curr_token}} }
+		url = 'http://'+auth_ip+'/v2.1/'+proj_id+'/servers'
+		headers = {'X-Auth-Token': curr_token }
 		try:
 			r = requests.get(url, headers=headers)
 			token=r.json()
