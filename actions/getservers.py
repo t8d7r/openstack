c80fb00f88d37ebd2d63c26cf96df930cdfd2be3
t8d7r <t8d7r@protonmail.com>
2017-05-08 03:48:47 -0400

    update

--- actions/getservers.py
+++ actions/getservers.py
@@ -14,10 +14,14 @@ class InternalDelIntf(Action):
 		keys = client.keys.get_all()
 		proj_id = client.keys.get_by_name(name='curr_project_id')
 		curr_token = client.keys.get_by_name(name='curr_token')
+		print proj_id.value+'\n'
+		print curr_token.value+'\n'
 		url = 'http://'+auth_ip+'/v2.1/'+proj_id.value+'/servers'
+		print url+'\n'
 		headers = {'X-Auth-Token': curr_token.value }
 		try:
 			r = requests.get(url, headers=headers)
+			print r
 			token=r.json()
 			length = len(token['servers'])
 			if serv_id == "default" :
